1caeb94e8985611a2c42ba4f210efbb466498fc4
Luijma <rsluismanuel@gmail.com>
2021-06-17 18:07:17 +0000

    Task 5 Attempt 1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+
+/**
+ * main - function using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
