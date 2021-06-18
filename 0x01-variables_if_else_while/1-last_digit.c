3b8618fff788ff098d1d96e80dcc0c67001167ac
Luijma <rsluismanuel@gmail.com>
2021-06-18 16:23:37 +0000

    Fixed logical errors in if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,13 +15,13 @@ int main(void)
 	lastDigit = n % 10;
 
 	printf("Last digit of %d is %d and ", n, lastDigit);
-	if (n > 5)
+	if (lastDigit > 5)
 	{
 		printf("is greater than 5");
-	} else if (n == 0)
+	} else if (lastDigit == 0)
 	{
 		printf("is 0");
-	} else if (n < 6 && n != 0)
+	} else if (lastDigit < 6)
 	{
 		printf("is less than 6 and not 0");
 	}
