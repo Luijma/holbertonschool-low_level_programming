7ae74636d3dddabd83b3262bab616eca4dec9de6
Luijma <rsluismanuel@gmail.com>
2021-06-18 20:16:17 +0000

    Task 8 attempt 1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - function that prints hexadecimal digits
+ * Return: 0
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit < 9; digit++)
+		putchar(digit);
+	for (digit = 'a'; digit <= 'f'; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
