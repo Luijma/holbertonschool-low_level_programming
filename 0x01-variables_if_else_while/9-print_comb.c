8065d0fe254aee4de60976a6ad7b92f62d4dd0ce
Luijma <rsluismanuel@gmail.com>
2021-06-19 00:27:56 +0000

    Task 9 attempt 1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - function that prints all digit comb
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+	int comma = 44;
+	int space = 32;
+
+	for (digit = 48; digit < 58; digit++)
+	{
+		putchar(digit);
+		if (digit != 57)
+		{
+			putchar(comma);
+			putchar(space);
+		}
+	}
+	return (0);
+}
