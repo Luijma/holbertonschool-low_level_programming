d36c100b45f8ad8e31d81cb8ff95cf762a5c813e
Luijma <rsluismanuel@gmail.com>
2021-06-22 22:02:58 +0000

    Testing theory regarding function files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,12 @@
-#include<stdio.h>
-void print_alphabet(void);
-/**
- * main - entry point of app
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+#include "holberton.h"
 /**
  * print_alphabet - prints alphabet in lowercase
  */
 void print_alphabet(void)
 {
-	char abc;
+        char abc;
 
-	for (abc = 'a'; abc <= 'z'; abc++)
-		putchar(abc);
-	putchar('\n');
+        for (abc = 'a'; abc <= 'z'; abc++)
+	       	_putchar(abc);
+       	_putchar('\n');
 }
