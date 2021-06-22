9380c93e1e47e1cc05a23bfde191a03050fda35d
Luijma <rsluismanuel@gmail.com>
2021-06-22 15:12:22 +0000

    Task 2 Attempt 1

--- 0x02-functions_nested_loops/2-print_alphbetx10.c
+++ 0x02-functions_nested_loops/2-print_alphbetx10.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
+void print_alphabet10(void);
 /**
  * main - entry point
  * Return: 0
  */
-void print_alph
+int main(void)
+{
+	print_alphabet10();
+	return (0);
+}
+/**
+ * print_alphabet10 - prints alphabet in lowercase 10 times
+ */
+void print_alphabet10(void)
+{
+	char abc;
+	int count = 0;
+
+	while (count < 11)
+	{
+		for (abc = 'a'; abc <= 'z'; abc++)
+			putchar(abc);
+		putchar('\n');
+	}
+}
