4a45511053b274f3858b9cd77e50baec6bdcfef3
Luijma <rsluismanuel@gmail.com>
2021-06-23 01:50:28 +0000

    Task 3 attempt 1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,7 @@
-#include<stdio.h>
+#include"holberton.h"
 /**
  * _islower - function that checks for letter case
  */
-int _islower(int c);
-/**
- * main - entry point for program
- */
-int main(void)
-{
-	int r;
-	r = _islower('H');
-	putchar(r);
-	return (0);
-}
 int _islower(int c)
 {
 	if(c < 123 && c > 96 )
