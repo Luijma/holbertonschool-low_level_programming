657cb60300e25dd530f30ca9a32484bb02485331
Luijma <rsluismanuel@gmail.com>
2021-06-23 03:34:51 +0000

    T8 A1

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints hours of day
+ */
+void jack_bauer(void)
+{
+	int x1 = 0;
+	int x2 = 0;
+	int y1 = 0;
+	int y2 = 0;
+
+	while (x1 < 3)
+	{
+		_putchar(x1 + '0');
+		_putchar(x2 + '0');
+		_putchar(':');
+		_putchar(y1 + '0');
+		_putchar(y2 + '0');
+		if (y2 != 10)
+		{
+			y2++;
+		}
+		if (y1 != 6 && y2 == 10)
+		{
+			y1++;
+			y2 = 0;
+		}
+		if (x2 != 4 && y1 == 6)
+		{
+			x2++;
+			y1 = 0;
+		}
+		if (x1 != 3 && x2 == 4)
+		{
+			x1++;
+			x2 = 0;
+		}
+	}
+}
