183057ad929c3990049c3bd2007b77d5e8d2e405
Luijma <rsluismanuel@gmail.com>
2021-06-23 01:46:54 +0000

    Fixed Documentation style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 void print_alphabet10(void);
 /**
- * print_alphabet10 - prints alphabet in lowercase 10 times
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
  */
 void print_alphabet_x10(void)
 {
