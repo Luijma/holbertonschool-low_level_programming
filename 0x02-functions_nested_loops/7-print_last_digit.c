778ea61999da71764ee948289aa82cb1e849e940
Luijma <rsluismanuel@gmail.com>
2021-06-23 02:34:46 +0000

    T7 A1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints final digit of n
+ * @n: number to extract last digit from
+ * Return: last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
