2f88a9ef30404beb9fbf5b4266b019d7a183b2be
Luijma <rsluismanuel@gmail.com>
2021-06-23 04:49:19 +0000

    T8 A3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,47 +1,37 @@
-#include "holberton.h"
+#include"holberton.h"
 /**
- * jack_bauer - prints hours of day
+ * jack_bauer - prints every minute
  */
 void jack_bauer(void)
 {
-	int x1 = 0;
-	int x2 = 0;
-	int y1 = 0;
-	int y2 = 0;
-	int max_of_x2 = 10;
-
+        int x1 = 0;
+        int x2 = 0;
+        int y1 = 0;
+        int y2 = 0;
+        int max_of_x2 = 10;
+	
 	while (x1 < 3)
 	{
-		_putchar(x1 + '0');
-		_putchar(x2 + '0');
-		_putchar(':');
-		_putchar(y1 + '0');
-		_putchar(y2 + '0');
-		_putchar('\n');
-		if (y2 != 10)
+		while (x2 != max_of_x2)
 		{
-			y2++;
-		}
-		if (y1 != 6 && y2 == 10)
-		{
-			y1++;
-			y2 = 0;
-		}
-		if (x2 != max_of_x2 && y1 == 6)
-		{
+			while(y1 != 6)
+			{
+				while (y2 != 10)
+				{
+					_putchar(x1 + '0');
+					_putchar(x2 + '0');
+					_putchar(':');
+					_putchar(y1 + '0');
+					_putchar(y2 + '0');
+					_putchar('\n');
+					y2++;
+				}
+				y1++;
+			}
 			x2++;
-			y1 = 0;
 		}
-		if (x2 > max_of_x2)
-		{
-			x1++;
-		}
-		if (x2 > max_of_x2 && x1 == 2)
-		{
-			x1++;
-			max_of_x2 = 3;
-			x2 = 0;
-		}
+	x1++;
+	if(x1 == 2)
+		max_of_x2 = 3;
 	}
-	_putchar('\n');
 }
