d01107cabc2c96a8eb736c2a8bdf3c53ff814a35
Luijma <rsluismanuel@gmail.com>
2021-06-27 23:19:50 +0000

    fixed trailing whitespace issue

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
@@ -7,10 +6,10 @@
  * @day: day of month
  * @year: year
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) ||
+			(year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -23,9 +22,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day -31, year);
-		}
-		else
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		} else
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\b", 365 - day);
