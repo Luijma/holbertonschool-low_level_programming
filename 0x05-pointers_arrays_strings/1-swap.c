43900a68615f89d72570c7d4be65d069f0de2884
Luijma <rsluismanuel@gmail.com>
2021-06-28 15:14:09 +0000

    Fixed betty style

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
  * swap_int - swaps the values of two integers
- * @a - first ingeger
- * @b - second integer
+ * @a: first integer
+ * @b: second integer
  */
 void swap_int(int *a, int *b)
 {
