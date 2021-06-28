6ea4f5278bc7bf4c2580b527f46faa52199e94be
Luijma <rsluismanuel@gmail.com>
2021-06-28 19:31:57 +0000

    T2A3, fixing compilation issue

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,10 +6,11 @@
  */
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 
-	for (length = 0; *s[length] != '\0'; length++)
+	while (*s[length] != '\0')
 	{
+		length++;
 	}
 	return (length);
 }
