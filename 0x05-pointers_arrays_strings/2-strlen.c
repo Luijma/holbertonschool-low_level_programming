e66dad290c2a1ce6743f9a92f60bf80b608cd09a
Luijma <rsluismanuel@gmail.com>
2021-06-28 19:16:05 +0000

    T2 A1, emtpy for

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - function that calculates length of a string
+ * @s: string to get length of
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; (s + length) != NULL; length++)
+	{
+	}
+	return (length);
+}
