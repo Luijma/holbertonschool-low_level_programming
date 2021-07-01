e87bf60517bb83c7106567efadccbe533e252fa8
Luijma <rsluismanuel@gmail.com>
2021-07-01 20:40:27 +0000

    T3 A3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,24 +3,12 @@
  * _strcmp.c - compares two strings
  * @@s1: first string
  * @s2: second string
- * Return: 15 if s1 > s2, 0 if ==, -15 if smaller
+ * Return: ascii values of s1 - s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1Length = 0;
-	int s2Length = 0;
-
-	while (*(s1 + s1Length))
-	{
-		s1Length++;
-	}
-	while (*(s2 + s2Length))
-	{
-		s2Length++;
-	}
-	if(s1Length > s2Length)
-		return 15;
-	else if(s1Length < s2Length)
-		return -15;
-	return 0;
+	int i;
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+	{}
+	return s1[i] - s2[i];
 }
