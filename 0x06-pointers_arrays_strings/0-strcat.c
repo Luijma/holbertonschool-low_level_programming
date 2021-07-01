7fc932d974b4e2a7a4b45e1e1d3e197f5b664619
Luijma <rsluismanuel@gmail.com>
2021-07-01 01:13:11 +0000

    T0 A1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,5 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char *new = dest;
+	int i;
+	int j;
 
+	for(i = 0; *(dest + i); i++)
+	{
+		*(new + i) = *(dest + i);
+	}
+	for(j = 0; *(src + j); j++)
+	{
+		*(new + i) = *(src + j);
+		i++;
+	}
+	*(new + i) = '\0';
+
+	return new;
 }
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,7 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-_putchar(char c);
+#include<stdio.h>
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
