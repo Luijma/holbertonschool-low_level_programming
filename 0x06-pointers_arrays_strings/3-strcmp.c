fc88f0639623ca473ba09c5c5aa339fa85a04928
Luijma <rsluismanuel@gmail.com>
2021-07-01 20:41:52 +0000

    T3 A4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
- * _strcmp.c - compares two strings
- * @@s1: first string
+ * _strcmp - compares two strings
+ * @s1: first string
  * @s2: second string
  * Return: ascii values of s1 - s2
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+
 	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
 	{}
-	return s1[i] - s2[i];
+	return (s1[i] - s2[i]);
 }
