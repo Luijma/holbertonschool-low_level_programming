95d4880cb4706d052f5030ecd54920b8f0a5ad49
Luijma <rsluismanuel@gmail.com>
2021-07-01 01:34:49 +0000

    T1 A1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,30 @@
  * REPLACE - prints string followed by new line
  * @str: string to be printed
  */
-void REPLACE(char *str)
+char *_strncat(char *dest, char *src, int n)
 {
-}
+	char *new = dest;
+	int srcLength = 0;
+	int i;
+	int j;
 
+	while(*(src + srcLength))
+	{
+		srcLength++;
+	}
+
+	for(i = 0; *(dest + i); i++)
+	{
+		*(new + i) = *(dest + i);
+	}
+	for(j = 0; (*(src + j) && (j < n)); j++)
+	{
+		*(new + i) = *(src + j);
+		i++;
+	}
+	if(srcLength < n)
+	{
+		*(new + i) = '\0';
+	}
+	return new;
+}
