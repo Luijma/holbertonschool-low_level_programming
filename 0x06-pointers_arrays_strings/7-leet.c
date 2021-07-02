09dea36f11ffb722ff4a428ee6c0f659f45c791e
Luijma <rsluismanuel@gmail.com>
2021-07-02 22:14:33 +0000

    Fixing checker 3 attempt

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * leet - encodes string into 1337
+ * @str - string to be passed
+ * Return: pointer to translated string
+ */
+char *leet(char *str)
+{
+	int i;
+	char leet[] = "aAeEoOtTlL";
+
+	for (i = 0; str[i]; i++)
+	{
+
+	}
+}
