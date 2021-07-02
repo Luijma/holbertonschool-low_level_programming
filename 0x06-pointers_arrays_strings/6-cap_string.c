fee436a71e7beb3052e89d25b429f4d0f4bc6709
Luijma <rsluismanuel@gmail.com>
2021-07-02 17:40:16 +0000

    Fixing weird bug attempt

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,40 @@
 #include "holberton.h"
 /**
- * cap_string - capitalizes all words of a string
- * @str: string to be checked and edited
- * Return: pointer to str
+ * main - check the code for Holberton School students.
+ * @a: an array of integers
+ * @n: the number of elements to swap
  */
-char *cap_string(char *str)
+void print_array(int *a, int n)
 {
 	int i;
-	int j = 0;
-	char seperator[] = " \t,;.!?\"(){}\n";
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	return (0);
+
 
 	for (i = 0; str[i]; i++)
 	{
