1fa7918b5b0e73a2ba9e1504db61b0be23465b75
Luijma <rsluismanuel@gmail.com>
2021-07-04 19:09:41 +0000

    fixed betty issues

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * leet - encodes string into 1337
- * @str - string to be passed
+ * @str: string to be passed
  * Return: pointer to translated string
  */
 char *leet(char *str)
