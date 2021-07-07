bb2637d7502a8564133465f08594f422102145bb
Luijma <rsluismanuel@gmail.com>
2021-07-07 04:20:57 +0000

    T3 A1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,15 +3,20 @@
  * _strspn - returns length of initial match
  * @s: string to be checked
  * @accept: letters to check for
- * Return number of bytes
+ * Return: number of bytes
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int r = 0;
 	int i;
 	int j;
+	int acceptLength = 0;
 
-	for (i = 0; s[i]; i++)
+	while (accept[acceptLength])
+	{
+		acceptLength++;
+	}
+	for (i = 0; i <= acceptLength; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
