44a963cd3ebde450b5870e54203733d7d999b025
Luijma <rsluismanuel@gmail.com>
2021-07-10 05:52:21 +0000

    T2 A2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,6 +5,19 @@
  * @argv: values passed
  * Return: 0 on success, 1 on failure
  */
-int main()
+int main(int argc, char *argv[])
 {
+	int i;
+	int letter;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (letter = 0; (*argv)[letter]; letter++)
+		{
+			_putchar((*argv)[letter]);
+		}
+		_putchar('\n');
+		argv++;
+	}
+	return (0);
 }
