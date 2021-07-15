86ce7c05626fab6be9a882c8324384be8d3f5cc6
Luijma <rsluismanuel@gmail.com>
2021-07-15 17:14:04 +0000

    T0 fixing betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,13 +2,14 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: amount of memory to be allocated
+ * Return: void pointer
  */
 void *malloc_checked(unsigned int b)
 {
-	void *array; 
+	void *array;
+
 	array = malloc(b);
-	
-	if(array == NULL)
+	if (array == NULL)
 		exit(98);
-	return array;
+	return (array);
 }
