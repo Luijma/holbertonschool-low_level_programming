3377b17035d0865e7624584abc788ebbf3070247
Luijma <rsluismanuel@gmail.com>
2021-07-15 02:04:05 +0000

    T4 A1

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * free_grid - frees a 2d array
+ * @grid: array to be freed
+ * @height: rows of array
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if(grid == NULL)
+		return;
+
+	for(i = 0; i < height; i++)
+	{
+		free((grid[i]));
+	}
+}
