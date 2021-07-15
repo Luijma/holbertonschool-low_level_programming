864ff74a46553f1e9cba93915791cc02940268b2
Luijma <rsluismanuel@gmail.com>
2021-07-15 17:47:34 +0000

    T1 A1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	newString = malloc(sizeof(char) * newLength);
 	if (newString == NULL)
 	{
-		free(newString);
 		return (NULL);
 	}
 	while (s1 && s1[i])
@@ -39,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		newString[i] = s1[i];
 		i++;
 	}
-	while (s2 && i < newLength)
+	while (s2 && s2[j] && i < newLength)
 	{
 		newString[i] = s2[j];
 		i++;
