afd0b037a89129585839d23291b509e4c334dacc
Luijma <rsluismanuel@gmail.com>
2021-07-15 00:16:36 +0000

    T2 Fixed Typo

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	}
 
-	while (s1 && s2[j])
+	while (s2 && s2[j])
 	{
 		newSize++;
 		j++;
@@ -38,7 +38,7 @@ char *str_concat(char *s1, char *s2)
 		newstr[i] = s1[i];
 		i++;
 	}
-	while (s2[j])
+	while (s2 && s2[j])
 	{
 		newstr[i] = s2[j];
 		i++;
