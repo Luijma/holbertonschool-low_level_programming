a8dfc1a2bd8ce2a44985fdf0e36c3b443387fb32
Luijma <rsluismanuel@gmail.com>
2021-07-19 23:51:53 +0000

    T2 A2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,12 +2,10 @@
 #include <stdio.h>
 /**
  * main - prints file you compiled from
- * @argc: argument count
- * @argv: pointer to arguments
  * Return: 0 on success
  */
 int main(void)
 {
-	printf("%s\n",__BASE_FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
