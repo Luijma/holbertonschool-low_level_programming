79823842801c7c3282c9b2efb3d24f652704a71d
Luijma <rsluismanuel@gmail.com>
2021-07-20 01:40:14 +0000

    T0 A1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog type
+ * @name: the doggo's name
+ * @age: the doggo's time spent on this earth
+ * @owner: the doggo's god
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+#endif /* DOG_H */
