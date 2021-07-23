c4cc8108b24f334157f7584a5dfc6a17b1b63f11
Luijma <rsluismanuel@gmail.com>
2021-07-23 00:31:43 +0000

    T0 A1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,2 +1,10 @@
 #include "holberton.h"
-
+/**
+ * print_name - uses function pointer to print a name
+ * @name: string to be printed
+ * @f: pointer to function that will print the string
+ */
+void print_name(char *name, void(*f)(char *))
+{
+	f(name);
+}
