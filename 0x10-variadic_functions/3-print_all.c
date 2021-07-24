0587f305144d424793a57df44cfb596bb580f052
Luijma <rsluismanuel@gmail.com>
2021-07-24 01:27:27 +0000

    T3 A5

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,29 +8,38 @@ void print_all(const char * const format, ...)
 	va_list argl;
 	int i = 0;
 	char *str;
+	int printed = 0;
 
 	va_start(argl, format);
-	while (format[i])
+	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(argl, int));
+				printed = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(argl, int));
+				printed = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(argl, double));
+				printed = 1;
 				break;
 			case 's':
 				str = va_arg(argl, char *);
 				if (str == NULL)
-					printf("nil");
-				if (str != NULL)
-					printf("%s", str);
+					str = "(nil)";
+				printf("%s", str);
+				printed = 1;
 				break;
 		}
+		if (printed && format[i + 1])
+		{
+			printf(", ");
+		}
+		printed = 0;
 		i++;
 	}
 	printf("\n");
