5323b6ffb160f0a45bd3e7a8017738db56be5ccf
Luijma <rsluismanuel@gmail.com>
2021-08-07 20:23:40 +0000

    T5 A1, probably forgot to make head null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if(!head)
+		return;
+
 	while (*head)
 	{
 		temp = *head;
 		*head = temp->next;
 		free(temp);
 	}
-	free(*head);
 }
