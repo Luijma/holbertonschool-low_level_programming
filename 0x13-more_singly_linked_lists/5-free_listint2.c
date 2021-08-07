a531d442bab5a32368ab65373ab999ef32dd125a
Luijma <rsluismanuel@gmail.com>
2021-08-07 20:09:00 +0000

    T5 A1

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees up the space for linked list members
+ * @head: head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
