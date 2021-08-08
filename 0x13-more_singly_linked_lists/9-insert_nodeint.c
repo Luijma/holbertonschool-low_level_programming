8e976634d5d861fe7b4606f3096b95b0b0775923
Luijma <rsluismanuel@gmail.com>
2021-08-08 00:20:29 +0000

    T9 A5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,9 +22,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
 	for (i = 0; i != idx; i++)
 	{
-		if (!temp->next || !temp)
+		if (!temp)
 		{
 			free(new);
 			return (NULL);
