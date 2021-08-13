0d502c6a2ebc3e71f61454d1c4e2645b49fe0884
Luijma <rsluismanuel@gmail.com>
2021-08-13 01:11:12 +0000

    T1 A1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - Creates a file and writes text onto it
+ * @filename: name of file to create
+ * @text_content: null terminated string to write to file
+ * Return: -1 on error, 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int txtlength = 0;
+	int printed;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[txtlength])
+	{
+		txtlength++;
+	}
+	printed = write(fd, text_content, txtlength);
+	if (printed == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
