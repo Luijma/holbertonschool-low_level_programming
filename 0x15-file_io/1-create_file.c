0d07c42de021c7340f6ffee79dfe5badaf85f7b9
Luijma <rsluismanuel@gmail.com>
2021-08-13 01:30:31 +0000

    T1 A4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,16 +14,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if(text_content == NULL)
+	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
 
-		close (fd);
+		close(fd);
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
