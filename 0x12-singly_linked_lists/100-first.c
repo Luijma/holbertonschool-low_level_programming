5136dd3db2efa94a1a53f03ee86152dfc6ace45b
Luijma <rsluismanuel@gmail.com>
2021-09-01 04:45:28 +0000

    Advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,8 @@
-#include "lists.h"
 #include <stdio.h>
 /**
  * first_function - function that runs before main
  */
-void first_function(void) __attribute__ ((constructor));
-void first_function(void)
+void __attribute__ ((constructor)) first_function(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
