1e736478410619c310b7d77930cd79014a519722
Luijma <rsluismanuel@gmail.com>
2021-09-01 04:10:42 +0000

    Task 0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of square's dimension
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+	int square = size * size;
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			_putchar('#');
+		}
+	}
+	_putchar('\n');
+}
