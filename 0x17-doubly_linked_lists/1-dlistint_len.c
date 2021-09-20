fb9d3f2a35ab5b9cbcb3d73ccc4cceeccec29850
Luijma <rsluismanuel@gmail.com>
2021-09-20 21:16:34 +0000

    T1 A1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - gets length of a doubly linked list
+ * @h: list to get length of
+ * Return: length of doubly linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+
+	while(h)
+	{
+		length++;
+		h = h->next;
+	}
+	return length;
+}
