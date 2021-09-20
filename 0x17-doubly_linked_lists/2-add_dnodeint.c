b7e4be916de0805889be9870075843df408123c6
Luijma <rsluismanuel@gmail.com>
2021-09-20 22:04:17 +0000

    Why is it failing

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,9 +14,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 
+	if (!*head)
+	{
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
+	} else
+	{
+		new_node->n = n;
+		new_node->prev = (*head)->prev;
+		new_node->next = *head
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
 	return (new_node);
 }
