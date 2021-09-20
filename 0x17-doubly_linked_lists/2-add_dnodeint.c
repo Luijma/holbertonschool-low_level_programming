74229784c9e8ba0b735f1a2017bb5bb67304cb11
Luijma <rsluismanuel@gmail.com>
2021-09-20 21:38:20 +0000

    T2 A1

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 
-	if(new_node->prev == NULL)
+	if (new_node->prev == NULL)
 	{
 		new_node->n = n;
 		new_node->next = *head;
