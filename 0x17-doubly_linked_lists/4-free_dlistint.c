dde6bce98d56eb39505799e9c4ba784f1c825b09
Luijma <rsluismanuel@gmail.com>
2021-09-20 22:42:12 +0000

    T4 A1

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head of list to free
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (!head)
+		return;
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
