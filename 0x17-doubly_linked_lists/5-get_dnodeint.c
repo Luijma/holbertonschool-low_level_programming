ac0380bd093dae9c8104dffa32537b2f5eb62358
Luijma <rsluismanuel@gmail.com>
2021-09-21 07:19:28 +0000

    T5 A1

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns node in index
+ * @head: head of the list to check index for
+ * @index: index of node to return
+ * Return: the node at index, NULL if it doesn't exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head,
+		unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *node;
+
+	if (!head)
+		return (NULL);
+	if (index < 0)
+		return (NULL);
+
+	node = head;
+
+	for (i = 0; i < index; i++)
+	{
+		if (node->next == NULL)
+		{
+			return (NULL);
+		}
+		node = node->next;
+	}
+	return (node);
+}
