f3a4d8c128bfb5dcd0f13a6614dde8b9f08cdadb
Luijma <rsluismanuel@gmail.com>
2021-09-21 09:05:00 +0000

    T7 A1

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert node in position
+ * @h: head of list
+ * @idx: index to insert
+ * @n: number node holds
+ * Return: NULL if impossible
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
+		unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *temp = *head;
+	unsigned int length = 0;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	while (temp)
+	{
+		length++;
+		temp = temp->next;
+	}
+
+	if (idx == 0)
+		return (add_dnodeint(head, n));
+	if (idx == length - 1)
+		return (add_dnodeint_end(head, n));
+	if (idx > length - 1)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	temp = get_dnodeint_at_index(*head, idx);
+	new_node->n = n;
+	new_node->prev = temp->prev;
+	temp->prev = new_node;
+	new_node->next = temp;
+	new_node->prev->next = new_node;
+	return (new_node);
+}
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int i = 0;
+
+	while (temp != NULL)
+	{
+		if (i == index)
+			return (temp);
+		i++;
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
