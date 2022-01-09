9e99d523ef4003a4fc0a27628c667a719531b847
Luijma <rsluismanuel@gmail.com>
2022-01-09 02:52:16 +0000

    fixed segmentation faults

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int length = 0;
 	dlistint_t *temp = *head;
 
-	if (!head)
+	if (!head || !*head)
 		return (-1);
 
 	/* Get length of dlist */
@@ -25,21 +25,42 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	/* deletion process */
 
-	temp = get_dnodeint_at_index(*head, index);
-
+	temp = get_node_indexed(*head, index);
 	if (index == 0)
 	{
+		printf("BEFORE POINTER MANIPULATION INDEX 0\n\n");
 		*head = temp->next;
-		temp->next->prev = NULL;
 	} else if (index == length)
 	{
 		temp->prev->next = NULL;
+		printf("INSIDE INDEX == LENGTH\n\n");
 	} else
 	{
 		temp->prev->next = temp->next;
 		temp->next->prev = temp->prev;
+		printf("INSIDE FINAL ELSE\n\n");
 	}
-
 	free(temp);
 	return (1);
+}
+/**
+ * get_node_indexed - gets node in index
+ * @head: head of list
+ * @index: index to search for
+ * Return: node, or NULL if it doesnt exist
+ */
+dlistint_t *get_node_indexed(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int i = 0;
+
+	while (temp != NULL)
+	{
+		if (i == index)
+			return (temp);
+		i++;
+		temp = temp->next;
+	}
+
+	return (NULL);
 }
