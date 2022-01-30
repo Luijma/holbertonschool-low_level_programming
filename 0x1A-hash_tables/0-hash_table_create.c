d6a3c87ea42094d1efa8706bf6d1aa1d027fab8d
Luijma <rsluismanuel@gmail.com>
2022-01-30 01:29:03 +0000

    fixed betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	new->array = calloc(size, sizeof(hash_node_t*));
+	new->array = calloc(size, sizeof(hash_node_t *));
 	if (new->array == NULL)
 	{
 		free(new);
