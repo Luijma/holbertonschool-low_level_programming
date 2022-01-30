32ed814ebf81fa755ea5f03cbfef351d106a7815
Luijma <rsluismanuel@gmail.com>
2022-01-30 00:11:35 +0000

    fixed betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,13 +4,13 @@
  * @size: size of list to be created
  * Return: pointer to newly created hash table
  */
-hash_table_t *hast_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
-	
+
 	new->array = malloc(sizeof(hash_node_s) * size);
 
-	if(new->array == NULL)
+	if (new->array == NULL)
 	{
 		return (NULL);
 	}
